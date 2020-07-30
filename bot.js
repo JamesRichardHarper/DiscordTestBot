2a51e2b8e34961c9758b27676971f4c1a544933d
James Harper <JHarper@academytrainee.com>
2020-07-30 17:54:39 +0100

    Added a few extra commands, enabled a prefix edit to change away from ! if needed

--- bot.js
+++ bot.js
@@ -3,26 +3,31 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var {prefix} = require('./config.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    if (message.substring(0, 1) == prefix) {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
@@ -33,6 +38,21 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
+                });
+            break;
+            // !Hello
+            case 'hello':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Oh, hi there!'
+                });
+            break;
+            //!roll20
+            case 'roll20':
+                let roll = Math.floor(Math.random() * 20) + 1;
+                bot.sendMessage({
+                    to: channelID,
+                    message: user + ' you rolled a ' + roll + ' out of 20.'
                 });
             break;
             // Just add any case commands if you want to..
